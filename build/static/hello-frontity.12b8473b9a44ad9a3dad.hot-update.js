ea9baf60d67960234f92d4f4d8dcf47df738a5d6
Kimberley Challis <kimchallis@gmail.com>
2021-05-21 13:46:37 +0100

    Initial commit

---
+++ build/static/hello-frontity.12b8473b9a44ad9a3dad.hot-update.js
@@ -0,0 +1,44 @@
+webpackHotUpdate("hello-frontity",{
+
+/***/ "./node_modules/@frontity/components/link/index.tsx":
+false,
+
+/***/ "./node_modules/@frontity/components/link/utils.ts":
+false,
+
+/***/ "./node_modules/@frontity/components/switch.tsx":
+false,
+
+/***/ "./node_modules/@frontity/hooks/use-in-view/index.ts":
+false,
+
+/***/ "./node_modules/core-js/modules/web.url.to-json.js":
+false,
+
+/***/ "./node_modules/dayjs/dayjs.min.js":
+false,
+
+/***/ "./node_modules/react-intersection-observer/react-intersection-observer.m.js":
+false,
+
+/***/ "./packages/my-first-theme/src/components/index.js":
+/*!*********************************************************!*\
+  !*** ./packages/my-first-theme/src/components/index.js ***!
+  \*********************************************************/
+/*! exports provided: default */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: /Users/kimberleychallis/GitHub/hello-frontity/packages/my-first-theme/src/components/index.js: Expected corresponding JSX closing tag for <Main>. (44:6)\\n\\n\\u001b[0m \\u001b[90m 42 |\\u001b[39m           \\u001b[33m<\\u001b[39m\\u001b[33mPage\\u001b[39m when\\u001b[33m=\\u001b[39m{data\\u001b[33m.\\u001b[39misPage} \\u001b[33m/\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 43 |\\u001b[39m         \\u001b[33m<\\u001b[39m\\u001b[33m/\\u001b[39m\\u001b[33mSwitch\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 44 |\\u001b[39m       \\u001b[33m<\\u001b[39m\\u001b[33m/\\u001b[39m\\u001b[33mmain\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m    |\\u001b[39m       \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 45 |\\u001b[39m     \\u001b[33m<\\u001b[39m\\u001b[33m/\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 46 |\\u001b[39m   )\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 47 |\\u001b[39m }\\u001b[33m;\\u001b[39m\\u001b[0m\\n    at Object._raise (/Users/kimberleychallis/GitHub/hello-frontity/node_modules/@babel/parser/src/parser/error.js:134:45)\\n    at Object.raiseWithData (/Users/kimberleychallis/GitHub/hello-frontity/node_modules/@babel/parser/src/parser/error.js:129:17)\\n    at Object.raise (/Users/kimberleychallis/GitHub/hello-frontity/node_modules/@babel/parser/src/parser/error.js:78:17)\\n    at Object.jsxParseElementAt (/Users/kimberleychallis/GitHub/hello-frontity/node_modules/@babel/parser/src/plugins/jsx/index.js:515:18)\\n    at Object.jsxParseElementAt (/Users/kimberleychallis/GitHub/hello-frontity/node_modules/@babel/parser/src/plugins/jsx/index.js:472:34)\\n    at Object.jsxParseElement (/Users/kimberleychallis/GitHub/hello-frontity/node_modules/@babel/parser/src/plugins/jsx/index.js:551:19)\\n    at Object.parseExprAtom (/Users/kimberleychallis/GitHub/hello-frontity/node_modules/@babel/parser/src/plugins/jsx/index.js:562:21)\\n    at Object.parseExprSubscripts (/Users/kimberleychallis/GitHub/hello-frontity/node_modules/@babel/parser/src/parser/expression.js:616:23)\\n    at Object.parseUpdate (/Users/kimberleychallis/GitHub/hello-frontity/node_modules/@babel/parser/src/parser/expression.js:596:21)\\n    at Object.parseMaybeUnary (/Users/kimberleychallis/GitHub/hello-frontity/node_modules/@babel/parser/src/parser/expression.js:563:23)\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL3BhY2thZ2VzL215LWZpcnN0LXRoZW1lL3NyYy9jb21wb25lbnRzL2luZGV4LmpzLmpzIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./packages/my-first-theme/src/components/index.js\n");
+
+/***/ }),
+
+/***/ "./packages/my-first-theme/src/components/list.js":
+false,
+
+/***/ "./packages/my-first-theme/src/components/page.js":
+false,
+
+/***/ "./packages/my-first-theme/src/components/post.js":
+false
+
+})
